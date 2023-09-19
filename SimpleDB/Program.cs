cd2d4c9c344b7620b39bb6f2a36f8b049c4c142d
Big Craudio <cl4udio.r4mos56@gmail.com>
2023-09-19 18:17:44 -0300

    📝 Insert e Busca
    Apesar de que eventualmente a gente vai precisar de uma cache, não acho que a implementação dela (como dicionário) é pra agora. Desse jeito a implementação é direta no arquivo

--- SimpleDB/Program.cs
+++ SimpleDB/Program.cs
@@ -1,23 +1,33 @@
 ﻿using System;
+using System.IO;
 
 namespace SimpleDB {
     class Program {
+        static string arquivoPath = "dados.txt"; // banco de dados
+
         static void Main(string[] args) {
             if (args.Length == 0) return;
             
-            string[] separado = args[0].Split('='); // [0] = comando, [1] = argumento
-            string[] valores = separado[1].Split(',');
+            string[] separado = args[0].Split('=');
 
+            string comando = separado[0];
+            string[] valores = separado[1].Split(','); // [0] = chave, [1] = valor
 
-            switch (separado[0]) {
+            switch (comando) {
                 case "--insert":
-                    Inserir(valores[0], valores[1]);
+                    if (Inserir(valores[0], valores[1])) Console.WriteLine("inserted");
+                    else Console.WriteLine("key already exists");
+
                     break;
                 case "--remove":
                     Remover(valores[0]);
                     break;
                 case "--search":
-                    Buscar(valores[0]);
+                    string? valor = Buscar(valores[0]);
+
+                    if (valor != null) Console.WriteLine(valor);
+                    else Console.WriteLine("not found");
+
                     break;
                 case "--update":
                     Atualizar(valores[0], valores[1]);
@@ -25,16 +35,47 @@ namespace SimpleDB {
             }
         }
 
-        static void Inserir(string chave, string valor) {
-            Console.WriteLine("Inserir [" + chave + "] = <" + valor + ">");
+        static bool Inserir(string chave, string valor) {
+            if (Buscar(chave) != null) {
+                return false;
+            }
+
+            StreamWriter arquivo = new StreamWriter(arquivoPath, true);
+
+            arquivo.BaseStream.Seek(0, SeekOrigin.End);
+            arquivo.WriteLine(chave + "," + valor);
+
+            arquivo.Close();
+
+            return true;
         }
 
         static void Remover(string chave) {
             Console.WriteLine("Remover [" + chave + "]");
         }
 
-        static void Buscar(string chave) {
-            Console.WriteLine("Buscar <" + chave + ">");
+        static string? Buscar(string chave) {
+            StreamReader arquivo = new StreamReader(arquivoPath);
+
+            if (arquivo == null || arquivo.EndOfStream) {
+                return null;
+            }
+
+            string? linha = arquivo.ReadLine();
+
+            while (linha != null) {
+                string[] separado = linha.Split(',');
+
+                if (separado[0] == chave) {
+                    return separado[1];
+                }
+
+                linha = arquivo.ReadLine();
+            }
+
+            arquivo.Close();
+
+            return null;
         }
 
         static void Atualizar(string chave, string novoValor) {
