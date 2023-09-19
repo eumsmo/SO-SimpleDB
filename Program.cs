f04b63ea6b008f47d37157db101cf931ca89c77f
Juan Vitor <juanriven10@gmail.com>
2023-09-18 22:43:05 -0300

    📂 Arquivos iniciais e linha de comando
    RFN-1 era sobre a interface de comando, então tá ai, só a interface de comando. Cada comando chama um método referente ao que devia fazer, porém os métodos ainda não foram implementados
    
    E o gitignore

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleDB {
+    class Program {
+        static void Main(string[] args) {
+            if (args.Length == 0) return;
+            
+            string[] separado = args[0].Split('='); // [0] = comando, [1] = argumento
+            string[] valores = separado[1].Split(',');
+
+
+            switch (separado[0]) {
+                case "--insert":
+                    Inserir(valores[0], valores[1]);
+                    break;
+                case "--remove":
+                    Remover(valores[0]);
+                    break;
+                case "--search":
+                    Buscar(valores[0]);
+                    break;
+                case "--update":
+                    Atualizar(valores[0], valores[1]);
+                    break;
+            }
+        }
+
+        static void Inserir(string chave, string valor) {
+            Console.WriteLine("Inserir [" + chave + "] = <" + valor + ">");
+        }
+
+        static void Remover(string chave) {
+            Console.WriteLine("Remover [" + chave + "]");
+        }
+
+        static void Buscar(string chave) {
+            Console.WriteLine("Buscar <" + chave + ">");
+        }
+
+        static void Atualizar(string chave, string novoValor) {
+            Console.WriteLine("Atualizar [" + chave + "] = <" + novoValor + ">");
+        }
+    }
+}
