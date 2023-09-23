9547a3b2229862117d0f5022143a241e5a4bebb6
Kuro <iaguinhodosantos2@gmail.com>
2023-09-23 15:19:16 -0300

    🚮 Remove e renomeia
    Pra remover uma linha você tem que criar um arquivo temporário que eventualmente será o arquivo original. Aí você repassa tudo que ta escrito do original pra ele só que você ignora a linha com o valor que você procurou.

--- SimpleDB/Program.cs
+++ SimpleDB/Program.cs
@@ -1,26 +1,32 @@
 ﻿using System;
 using System.IO;
 
-namespace SimpleDB {
-    class Program {
-        static string arquivoPath = "dados.txt"; // banco de dados
+namespace SimpleDB
+{
+    class Program
+    {
+        const string arquivoPath = "simpledb.db"; // banco de dados
+        const string tempPrefix = "temp_"; // prefixo para arquivos temporários
 
-        static void Main(string[] args) {
+        static void Main(string[] args)
+        {
             if (args.Length == 0) return;
-            
+
             string[] separado = args[0].Split('=');
 
             string comando = separado[0];
             string[] valores = separado[1].Split(','); // [0] = chave, [1] = valor
 
-            switch (comando) {
+            switch (comando)
+            {
                 case "--insert":
                     if (Inserir(valores[0], valores[1])) Console.WriteLine("inserted");
                     else Console.WriteLine("key already exists");
 
                     break;
                 case "--remove":
-                    Remover(valores[0]);
+                    if (Remover(valores[0])) Console.WriteLine("removed");
+                    else Console.WriteLine("not found");
                     break;
                 case "--search":
                     string? valor = Buscar(valores[0]);
@@ -35,8 +41,10 @@ namespace SimpleDB {
             }
         }
 
-        static bool Inserir(string chave, string valor) {
-            if (Buscar(chave) != null) {
+        static bool Inserir(string chave, string valor)
+        {
+            if (Buscar(chave) != null)
+            {
                 return false;
             }
 
@@ -50,23 +58,68 @@ namespace SimpleDB {
             return true;
         }
 
-        static void Remover(string chave) {
-            Console.WriteLine("Remover [" + chave + "]");
-        }
+        static bool Remover(string chave)
+        {
+            string tempPath = tempPrefix + arquivoPath;
+            bool removeu = false;
 
-        static string? Buscar(string chave) {
+            // Cria um arquivo temporário
+            StreamWriter temp_arquivo = new StreamWriter(tempPath, true);
             StreamReader arquivo = new StreamReader(arquivoPath);
 
-            if (arquivo == null || arquivo.EndOfStream) {
+            if (arquivo == null || arquivo.EndOfStream || temp_arquivo == null)
+            {
+                return false;
+            }
+
+            string? linha = arquivo.ReadLine();
+
+            while (linha != null)
+            {
+                string[] separado = linha.Split(',');
+
+                // Passa linha por linha para o arquivo temporário e se a chave for igual, ignora
+                if (separado[0] == chave) removeu = true;
+                else temp_arquivo.WriteLine(linha);
+
+                linha = arquivo.ReadLine();
+            }
+
+            arquivo.Close();
+            temp_arquivo.Close();
+
+            if (removeu)
+            {
+                // Substitui o arquivo original pelo temporário
+                File.Delete(arquivoPath);
+                File.Move(tempPath, arquivoPath);
+            }
+            else
+            {
+                // Remove o arquivo temporário
+                File.Delete(tempPath);
+            }
+
+            return removeu;
+        }
+
+        static string? Buscar(string chave)
+        {
+            StreamReader arquivo = new StreamReader(arquivoPath);
+
+            if (arquivo == null || arquivo.EndOfStream)
+            {
                 return null;
             }
 
             string? linha = arquivo.ReadLine();
 
-            while (linha != null) {
+            while (linha != null)
+            {
                 string[] separado = linha.Split(',');
 
-                if (separado[0] == chave) {
+                if (separado[0] == chave)
+                {
                     return separado[1];
                 }
 
@@ -78,7 +131,8 @@ namespace SimpleDB {
             return null;
         }
 
-        static void Atualizar(string chave, string novoValor) {
+        static void Atualizar(string chave, string novoValor)
+        {
             Console.WriteLine("Atualizar [" + chave + "] = <" + novoValor + ">");
         }
     }
