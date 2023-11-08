62e65b299b16366581bbb5081715281aef014b7a
MaryLife <112979364+MaryLife@users.noreply.github.com>
2023-11-08 17:24:26 -0300

    📁Base cliente
    - Atualmente essa é a base para o sistema do cliente, onde teremos um input básico. Porém, ela ainda não está checando ou até mesmo lendo valores, só realizando comandos.

---
+++ SimpleDBClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+namespace SimpleDBClient 
+{
+    class Program 
+    {
+        static void Main(string[] args) 
+        {
+            while(true) 
+            {
+                string? entrada = Console.ReadLine();
+
+                if (entrada == null) continue;
+
+                string[] entradaSeparada = entrada.Split(" ", 2);
+                string metodo = entradaSeparada[0];
+                string retorno = "command does not exist";
+
+                switch(metodo) 
+                {
+                    case "insert":
+                        retorno = Inserir();
+                        break;
+                    case "remove":
+                        retorno = Remover();
+                        break;
+                    case "search":
+                        retorno = Buscar();
+                        break;
+                    case "update":
+                        retorno = Atualizar();
+                        break;
+                    case "quit":
+                        return;
+                }
+
+                Console.WriteLine(retorno);
+            }
+        }
+
+        static string Inserir()
+        {
+            return "inserted";
+        }
+
+        static string Remover()
+        {
+            return "removed";
+        }
+
+        static string Buscar()
+        {
+            return "valeu here";
+        }
+
+        static string Atualizar()
+        {
+            return "updated";
+        }
+    }
+}
